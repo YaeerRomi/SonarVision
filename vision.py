52fc56e99858bdafbca36ec10a45ffb5204419df
Kalen <110569039+KalenHermalin@users.noreply.github.com>
2023-03-30 13:59:24 -0400

    Updated Vision More

--- vision.py
+++ vision.py
@@ -8,19 +8,20 @@ cam = cv2.VideoCapture(0)
 lower_red = np.array([0,130,60])
 upper_red = np.array([10,255,255])
 
-
 while True:
-    ret, frame = cam.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-
-
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow("camera", frame)
-    cv2.imshow("Result", result)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
+    ret,frame=cam.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    mask=cv2.inRange(hsv,lower_red,upper_red)
+    _,mask1=cv2.threshold(mask,254,255,cv2.THRESH_BINARY)
+    cnts,_=cv2.findContours(mask1,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for c in cnts:
+        x=600
+        if cv2.contourArea(c)>x:
+            x,y,w,h=cv2.boundingRect(c)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    
+    cv2.imshow("FRAME",frame)
+    if cv2.waitKey(1) == ord("q"):
         break
-
 cam.release()
 cv2.destroyAllWindows()
