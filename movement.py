d125a0c8683ab54afbf9773bef555c58dab160fe
YaeerRomi <yaeerromi@gmail.com>
2023-04-03 18:53:50 -0400

    Update Movement.py

--- movement.py
+++ movement.py
@@ -1,47 +1,39 @@
 import time
-from gpriozero import Robot, Motor
+import math
+import keyboard
+from RPi.GPIO import GPIO
+from gpiozero import Robot
 
-car = Robot(leftMotor=(pin1, pin2), rightMotor=(pin3, pin4))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+# Left Motor
+GPIO.setup(16, GPIO.OUT)
+GPIO.steup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+# Right Motor
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+# GPIOZERO METHOD
+
+tesla = Robot(left=(16, 18), right=(11, 13))
 
 
-left_motor_pin = ""
-right_motor_pin = ""
+def motors():
+    tesla = Robot(left=(16, 18), right=(11, 13))
 
-acceloration = 0.1
-speed = 0
-forward_maxSpeed = 100
-backward_maxSpeed = 30
+    while True:
+        if keyboard.is_pressed('w'):
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(left_motor_pin, GPIO.OUT)
-GPIO.setup(right_motor_pin, GPIO.OUT)
+            tesla.forward()
+        elif keyboard.is_pressed('s'):
+            tesla.backward()
 
-def move_forward(maxSpeed, speed, accelor):
-    
-    while speed <= maxSpeed:
-        leftMotor.forward(speed)
-        rightMotor.forward(speed)
-        time.speed(0.5)
-        speed += accelor
-    
-    moveForward = True
-        
-def move_backward(maxSpeed, speed, accelor):
-    
-    while speed >= maxSpeed:
-        leftMotor.backward(speed)
-        rightMotor.backward(speed)
-        time.speed(0.5)
-        speed -= accelor
-    
-        
-        
-    
-    
+        else:
+            tesla.stop()
 
-while True:
-    command = input("Enter a command: ")
-    if command == "go":
-        move_forward()
-    else:
-        print("Invalid command. Try again.")
+
+motors()
