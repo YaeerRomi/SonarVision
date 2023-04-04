358e88fe050806ebccacb3361609ee2549a04205
YaeerRomi <yaeerromi@gmail.com>
2023-04-04 14:54:18 -0400

    Movement.py Update

--- movement.py
+++ movement.py
@@ -1,47 +1,50 @@
 import time
-from gpriozero import Robot, Motor
+import RPi.GPIO as GPIO
 
-car = Robot(leftMotor=(pin1, pin2), rightMotor=(pin3, pin4))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-
-left_motor_pin = ""
-right_motor_pin = ""
-
-acceloration = 0.1
 speed = 0
-forward_maxSpeed = 100
-backward_maxSpeed = 30
+acceleration = 0.1
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(left_motor_pin, GPIO.OUT)
-GPIO.setup(right_motor_pin, GPIO.OUT)
+IN1 = 16
+IN2 = 18
+ENA = 22
+IN3 = 11
+IN4 = 13
+ENB = 15
 
-def move_forward(maxSpeed, speed, accelor):
-    
-    while speed <= maxSpeed:
-        leftMotor.forward(speed)
-        rightMotor.forward(speed)
-        time.speed(0.5)
-        speed += accelor
-    
-    moveForward = True
-        
-def move_backward(maxSpeed, speed, accelor):
-    
-    while speed >= maxSpeed:
-        leftMotor.backward(speed)
-        rightMotor.backward(speed)
-        time.speed(0.5)
-        speed -= accelor
-    
-        
-        
-    
-    
 
-while True:
-    command = input("Enter a command: ")
-    if command == "go":
-        move_forward()
-    else:
-        print("Invalid command. Try again.")
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+
+direct  = (input("Input Direction: ")).lower()
+speed = int(input("Input Speed:"))
+
+pwn = GPIO.PWN(ENA, 50)
+
+if direct == "right":
+    GPIO.output(IN1, 1)
+    GPIO.output(IN2, 0)
+    GPIO.output(IN3, 0)
+    GPIO.output(IN4, 0)
+elif direct == "left":
+    GPIO.output(IN1, 0)
+    GPIO.output(IN2, 0)
+    GPIO.output(IN3, 1)
+    GPIO.output(IN4, 0)
+    
+try:
+    while True:
+        pwn.start(speed)
+        
+except KeyboardInterrupt:
+    pwn.stop()
+    GPIO.cleanup()
+    
