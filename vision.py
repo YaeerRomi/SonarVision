df2a9a38fa540d74072f9155a7765c37e987fd92
Kalen <110569039+KalenHermalin@users.noreply.github.com>
2023-04-05 14:30:01 -0400

    Vision Updated
    Updated Vision To Detect Stop Signs

--- vision.py
+++ vision.py
@@ -2,25 +2,22 @@ import cv2
 import numpy as np
 cam = cv2.VideoCapture(0)
 
-# np.array([0,140,140]) LIGHT REDS
-# lower mask (0-10) lower_red = np.array([0,70,53])
+stop_data = cv2.CascadeClassifier('stop_data.xml')
 
-lower_red = np.array([0,130,60])
-upper_red = np.array([10,255,255])
 
 while True:
     ret,frame=cam.read()
-    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    mask=cv2.inRange(hsv,lower_red,upper_red)
-    _,mask1=cv2.threshold(mask,254,255,cv2.THRESH_BINARY)
-    cnts,_=cv2.findContours(mask1,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for c in cnts:
-        x=600
-        if cv2.contourArea(c)>x:
-            x,y,w,h=cv2.boundingRect(c)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    found = stop_data.detectMultiScale(gray_frame, 
+                                   minSize =(20, 20))
+    
+    if len(found) != 0:
+        for (x, y, width, height) in found:
+            cv2.rectangle(frame, (x,y), (x + width,y + height), (0, 255, 0), 5)
     
     cv2.imshow("FRAME",frame)
+
     if cv2.waitKey(1) == ord("q"):
         break
 cam.release()
