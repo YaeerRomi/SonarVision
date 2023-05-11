75542196874d2efa22bc5e828eb33c8826bd0598
YaeerRomi <yaeerromi@gmail.com>
2023-05-11 13:58:15 -0400

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,89 +1,32 @@
 import time
 import RPi.GPIO as GPIO
+from robot_hat import motor
 import keyboard
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-
-# Motor 1 Pins
-IN1 = 16
-IN2 = 18
-ENA = 22
-
-# Motor 2 Pins
-IN3 = 11
-IN4 = 13
-ENB = 15
-
-
-class Car:
-    def __init__(self, IN1, IN2, ENA, IN3, IN4, ENB):
-        # Initialize Motor 1
-        self.GPIO.setup(IN1, GPIO.OUT)
-        self.GPIO.setup(IN1, GPIO.OUT)
-        self.GPIO.setup(ENA, GPIO.OUT)
-        self.PWMA = GPIO.PWM(ENA, 100)
-        self.PWMA.start(0)
-
-        # Initialize Motor 2
-        self.GPIO.setup(IN3, GPIO.OUT)
-        self.GPIO.setup(IN4, GPIO.OUT)
-        self.GPIO.setup(ENB, GPIO.OUT)
-        self.PWMB = GPIO.PWM(ENB, 100)
-        self.PWMB.start(0)
-    
-    def drive_forward(self, speed):
-        # Motor 1 Speed
-        self.PWMA.ChangeDutyCycle(speed)
-        # Motor 2 Speed
-        self.PWMB.ChangeDutyCycle(speed)
+class Movement:
+    def __init__(self):
+        self.motor = motor.Motor()
         
-        # Motor 1 Forward
-        self.GPIO.output(IN1, GPIO.HIGH)
-        self.GPIO.output(IN2, GPIO.LOW)
-        # Motor 2 Forward
-        self.GPIO.output(IN3, GPIO.HIGH)
-        self.GPIO.output(IN4, GPIO.LOW)
+    def forward(self, speed):
+        self.motor.wheel(speed, motor=-1)
         
-    def drive_backward(self, speed):
-        # Motor 1 Speed
-        self.PWMA.ChangeDutyCycle(speed)
-        # Motor 2 Speed
-        self.PWMB.ChangeDutyCycle(speed)
-        
-        # Motor 1 Reverse
-        self.GPIO.output(IN1, GPIO.LOW)
-        self.GPIO.output(IN2, GPIO.HIGH)
-        # Motor 2 Reverse
-        self.GPIO.output(IN3, GPIO.LOW)
-        self.GPIO.output(IN4, GPIO.HIGH)
-    
-    def drive_stop(self):
-        # Motor 1 Speed
-        self.PWMA.ChangeDutyCycle(0)
-        # Motor 2 Speed
-        self.PWMB.ChangeDutyCycle(0)
-        
-        # Motor 1 Stop
-        self.GPIO.output(IN1, GPIO.LOW)
-        self.GPIO.output(IN2, GPIO.LOW)
-        # Motor 2 Stop
-        self.GPIO.output(IN3, GPIO.LOW)
-        self.GPIO.output(IN4, GPIO.LOW)
-        
-        
-        
-car = Car(IN1, IN2, ENA, IN3, IN4, ENB)
+    def backward(self, speed):
+        self.motor.wheel(-speed, motor=-1)
 
-forward = car.drive_forward()
+    def stop(self, speed):
+        self.motor.wheel(0, motor=-1)
 
-reverse  = car.drive_backward()
-
-while True:
-    if keyboard.is_pressed("w"):
-        car.drive_forward(50)
-    if keyboard.is_pressed("s"):
-        car.drive_backward(50)
-    if keyboard.is_pressed("x"):
-        car.drive_stop()
-        
+    def accelerate(self, start_speed, end_speed, duration):
+        step = (end_speed - start_speed) / duration
+        for speed in range(start_speed, end_speed, int(step)):
+            self.forward(speed)
+            time.sleep(0.1)
+            
+    def decelerate(self, start_speed, end_speed, duration):
+        step = (start_speed - end_speed) / duration
+        for speed in range(start_speed, end_speed, -int(step)):
+            self.forward(speed)
+            time.sleep(0.1)
+            
+    def turns(self, speed, angle):
+        pass
